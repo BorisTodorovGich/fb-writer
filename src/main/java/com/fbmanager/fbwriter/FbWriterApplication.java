2407a840a2d336c80d255cb3368c5d36cf451ca4
boris.gichev <boris.gichev@gmail.com>
2018-07-03 22:46:36 +0300

    FBW-1 spring fresh project with one test rest template

---
+++ src/main/java/com/fbmanager/fbwriter/FbWriterApplication.java
@@ -0,0 +1,12 @@
+package com.fbmanager.fbwriter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FbWriterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FbWriterApplication.class, args);
+	}
+}

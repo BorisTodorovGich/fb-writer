45e6f61312d48f999d803ce924e5b2adef36b759
boris.gichev <boris.gichev@gmail.com>
2018-07-05 14:32:23 +0300

    FBW-2 added thymleaf with bootstrap and jquery webjars

--- src/main/java/com/fbmanager/fbwriter/controller/Test.java
+++ src/main/java/com/fbmanager/fbwriter/controller/Test.java
@@ -1,15 +1,22 @@
 package com.fbmanager.fbwriter.controller;
 
+import javafx.scene.input.DataFormat;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+import java.util.Date;
+
+@Controller
 public class Test {
 
     @GetMapping(value = "/test")
-    public String test(){
-        return "heloo";
+    public String test(Model model){
+
+        model.addAttribute("serverTime", new Date());
+
+        return "test";
     }
 
 }
